b39c0dee5307481996746d7100cb56141e523cf8
alkcode <alek_1604@comunidad.unam.mx>
2023-02-13 17:49:18 -0600

    Validacion de form gestion-usuarios

--- src/app/components/gestion-guardias/gestion-guardias.component.ts
+++ src/app/components/gestion-guardias/gestion-guardias.component.ts
@@ -1,10 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-gestion-guardias',
   templateUrl: './gestion-guardias.component.html',
   styleUrls: ['./gestion-guardias.component.css']
 })
-export class GestionGuardiasComponent {
+export class GestionGuardiasComponent{
 
+  @Input() clave='';
+  @Input() contrasena='';
+  @Input() contrasena2='';
+  @Input() nombre='';
+  @Input() perfil='';
+
+  ngOnIniti(){
+    alert(this.clave);
+  }
 }
--- src/app/components/gestion-usuarios/gestion-usuarios.component.ts
+++ src/app/components/gestion-usuarios/gestion-usuarios.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-gestion-usuarios',
@@ -6,5 +7,48 @@ import { Component } from '@angular/core';
   styleUrls: ['./gestion-usuarios.component.css']
 })
 export class GestionUsuariosComponent {
+
+  formUsuario: FormGroup = new FormGroup({});
+  clave='';
+  contasena='';
+  contasena2='';
+  nombre='';
+  
+
+  mostrarContrasena: boolean = false;
+  mostrarContrasena2: boolean = false;
+
+  perfiles = [
+
+    {nombre: 'Guardias', valor: '1'},
+    {nombre: 'Suplencias', valor: '2'},
+    {nombre: 'Presupuesto', valor: '3'},
+  ];
+
+  ngOnInit(){
+    this.formUsuario = new FormGroup({
+      clave: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      contrasena: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      contrasena2: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      empleado: new FormControl('', [Validators.required, Validators.minLength(6)]),
+      // perfil: new FormControl(this.perfiles[0])
+      perfil: new FormControl(null, [Validators.required])
+    });
+
+  }
+
+  cambiarContrasena() {
+    this.mostrarContrasena = !this.mostrarContrasena;
+  }
+
+  cambiarContrasena2() {
+    this.mostrarContrasena2 = !this.mostrarContrasena2;
+  }
+  
+
+  // validarDatos(){
+  //   const body = this.formUsuario.value;
+  //   console.log(body);
+  // }
 
 }
